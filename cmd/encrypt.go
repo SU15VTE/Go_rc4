ef9f9f94e5e0258357611e556e26ca49fea365c7
SU15VTE <YuiFanjiayue@protonmail.com>
2023-04-21 19:22:30 +0800

    upload github

--- cmd/encrypt.go
+++ cmd/encrypt.go
@@ -16,10 +16,6 @@ var encryptCmd = &cobra.Command{
 	Short: "rc4 encrypt",
 	Long:  `nil`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("encrypt called")
-		fmt.Println("en called")
-		fmt.Printf("Key:	%s\n", key)
-		fmt.Printf("text:	%s\n", text)
 		result := RC4_encrypt(String_to_byte(key), String_to_byte(text))
 		fmt.Printf("ciphertext: %s\n", string(result))
 	},
